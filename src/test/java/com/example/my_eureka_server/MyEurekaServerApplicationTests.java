226d156fee86aa7405ce1c297b1cde045bb28cf3
WasimShaik17 <231fa18242@gmail.com>
2026-04-21 19:58:35 +0530

    microservices project (eureka server)

---
+++ src/test/java/com/example/my_eureka_server/MyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
